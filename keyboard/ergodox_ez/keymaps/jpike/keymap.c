e60b288d2a4a0e993e6b60e0620ef5880123a624
James Pike <jpike@chilon.net>
2016-04-22 14:37:44 +0100

    make more symmetrical

--- keyboard/ergodox_ez/keymaps/jpike/keymap.c
+++ keyboard/ergodox_ez/keymaps/jpike/keymap.c
@@ -16,20 +16,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   =    |   1  |   2  |   3  |   4  |   5  |  L1  |           |  L1  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | LGui |           | LGui |   Y  |   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |Bkspc/|           |Bkspc/|   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------| LGui |           | LGui |------+------+------+------+------+--------|
  * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|Ctrl/'  |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|Ret/L1|           |Ret/L1|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |  ~   |  '"  |  L2  | Left | Right|                                       | Down |  Up  |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        |Del/L1| Lalt |       | Ralt |Bspc/L1 |
+ *                                        |Hyper | Lalt |       | Ralt | Hyper  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 | Space| Tab  |------|       |------|  Tab   |Space |
+ *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -37,24 +37,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   TG(SYMB),
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LGUI,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   GUI_T(KC_BSPC),
         CTL_T(KC_ESC),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   LT(SYMB, KC_ENT),
         KC_GRV,         KC_QUOT,   TG(MDIA), KC_LEFT, KC_RGHT,
 
-                                                         LT(SYMB,KC_DELT),  KC_LALT,
+                                                                  KC_HYPR,  KC_LALT,
                                                                             KC_HOME,
-                                                           KC_SPC, KC_BSPC, KC_END,
+                                                           KC_SPC, KC_TAB,  KC_END,
         // right hand
         TG(SYMB),    KC_6,   KC_7,    KC_8,    KC_9,   KC_0,             KC_MINS,
-        KC_LGUI,     KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,             KC_BSLS,
+  GUI_T(KC_BSPC),    KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,             KC_BSLS,
                      KC_H,   KC_J,    KC_K,    KC_L,   LT(MDIA, KC_SCLN),CTL_T(KC_QUOT),
-        MEH_T(KC_NO),KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,          KC_RSFT,
+LT(SYMB, KC_ENT),    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,          KC_RSFT,
                              KC_DOWN, KC_UP,   KC_LBRC,KC_RBRC,          KC_FN1,
 
-        KC_RALT,         LT(SYMB,KC_BSPC),
+        KC_RALT,         KC_HYPR,
         KC_PGUP,
-        KC_PGDN, KC_TAB, KC_ENT
+        KC_PGDN, KC_TAB, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
