6e4964aed0ece987f9ed7ed8afe93bc11af3ec50
James Pike <jpike@chilon.net>
2019-04-07 14:30:21 +0100

    works against latest master + add reset and delete mappings

--- keyboards/ergodox_ez/keymaps/jpike/keymap.c
+++ keyboards/ergodox_ez/keymaps/jpike/keymap.c
@@ -1,10 +1,15 @@
-#include "ergodox_ez.h"
-#include "debug.h"
-#include "action_layer.h"
+#include QMK_KEYBOARD_H
+#include "version.h"
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
+
+enum custom_keycodes {
+  EPRM = SAFE_RANGE,
+  VRSN,
+  RGB_SLD
+};
 
 #define KC_QSTN  LSFT(KC_SLSH)
 #define KC_LANG  LSFT(KC_COMM)
@@ -36,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
-[BASE] = KEYMAP(  // layer 0 : default
+[BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_TPSTE,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   GUI_T(KC_BSPC),
@@ -63,7 +68,7 @@ LT(SYMB, KC_TAB),    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,          KC_RSFT
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |  #   |   &  |  |   |  \   |      |      |           |      |      |   [  |   <  |   >  |  ]   |        |
+ * |        |  #   |   &  |  |   |  \   |      |      |           |      | RESET|   [  |   <  |   >  |  ]   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |  ~   |   _  |  =   |  /   |      |------|           |------| DEL  |   {  |   (  |   )  | }    |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -80,7 +85,7 @@ LT(SYMB, KC_TAB),    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,          KC_RSFT
  *                                 `--------------------'       `--------------------'
  */
 // SYMBOLS
-[SYMB] = KEYMAP(
+[SYMB] = LAYOUT_ergodox(
        // left hand
        KC_TRNS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_TRNS,
        KC_TRNS,   KC_HASH, KC_AMPR, KC_PIPE, KC_BSLS, KC_TRNS,   KC_TRNS,
@@ -93,7 +98,7 @@ LT(SYMB, KC_TAB),    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,          KC_RSFT
                                                KC_TRNS, KC_TRNS, KC_TRNS,
        // right hand
        KC_TRNS,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_F11,
-       KC_TRNS,   KC_TRNS, KC_LBRC, KC_LANG, KC_RANG, KC_RBRC,   KC_TRNS,
+       KC_TRNS,   RESET,   KC_LBRC, KC_LANG, KC_RANG, KC_RBRC,   KC_TRNS,
                   KC_DEL,  KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR,   KC_TRNS,
        KC_TRNS,   KC_TRNS, KC_PLUS, KC_MINS, KC_ASTR, KC_TRNS,   KC_TRNS,
                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,
@@ -124,7 +129,7 @@ LT(SYMB, KC_TAB),    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,          KC_RSFT
  *                                 `--------------------'       `--------------------'
  */
 // MEDIA AND MOUSE
-[MDIA] = KEYMAP(
+[MDIA] = LAYOUT_ergodox(
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
@@ -134,7 +139,7 @@ LT(SYMB, KC_TAB),    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,          KC_RSFT
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
                                   KC_TRNS, KC_TRNS, KC_TRNS,
-    // right hand
+       // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_BTN1, KC_BTN2, KC_PSTE, KC_TRNS, KC_TRNS,
                  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_MPLY,
@@ -147,50 +152,100 @@ LT(SYMB, KC_TAB),    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,          KC_RSFT
 ),
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
-};
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-        if (record->event.pressed) {
-          register_code(KC_RSFT);
-        } else {
-          unregister_code(KC_RSFT);
-        }
-        break;
-      }
-    return MACRO_NONE;
-};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch (keycode) {
+      case EPRM:
+        eeconfig_init();
+        return false;
+      case VRSN:
+        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        return false;
+      #ifdef RGBLIGHT_ENABLE
+      case RGB_SLD:
+        rgblight_mode(1);
+        return false;
+      #endif
+    }
+  }
+  return true;
+}
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-
+#ifdef RGBLIGHT_COLOR_LAYER_0
+  rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+#endif
 };
 
-// Runs constantly in the background, in a loop.
-void matrix_scan_user(void) {
+// Runs whenever there is a layer state change.
+uint32_t layer_state_set_user(uint32_t state) {
+  ergodox_board_led_off();
+  ergodox_right_led_1_off();
+  ergodox_right_led_2_off();
+  ergodox_right_led_3_off();
 
-    uint8_t layer = biton32(layer_state);
-
-    ergodox_board_led_off();
-    ergodox_right_led_1_off();
-    ergodox_right_led_2_off();
-    ergodox_right_led_3_off();
-    switch (layer) {
-      // TODO: Make this relevant to the ErgoDox EZ.
-        case 1:
-            ergodox_right_led_1_on();
-            break;
-        case 2:
-            ergodox_right_led_2_on();
-            break;
-        default:
-            // none
-            break;
+  uint8_t layer = biton32(state);
+  switch (layer) {
+      case 0:
+        #ifdef RGBLIGHT_COLOR_LAYER_0
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+        #else
+        #ifdef RGBLIGHT_ENABLE
+          rgblight_init();
+        #endif
+        #endif
+        break;
+      case 1:
+        ergodox_right_led_1_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_1
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
+        #endif
+        break;
+      case 2:
+        ergodox_right_led_2_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_2
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
+        #endif
+        break;
+      case 3:
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_3
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
+        #endif
+        break;
+      case 4:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_4
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
+        #endif
+        break;
+      case 5:
+        ergodox_right_led_1_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_5
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
+        #endif
+        break;
+      case 6:
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_6
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+        #endif
+        break;
+      case 7:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_7
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_7);
+        #endif
+        break;
+      default:
+        break;
     }
 
+  return state;
 };
