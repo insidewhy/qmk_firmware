e15f01bc6049df6b51bb5f500f17c07f4e75747b
James Pike <jpike@chilon.net>
2021-10-24 15:03:06 +0800

    add instructions for initialising repo to doit.sh

--- doit.sh
+++ doit.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# before running this:
+#  - pikaur -S python-pyusb python-milc qmk
+#  - make git-submodule
+#  - qmk clean
 make ergodox_ez:jpike && teensy-loader-cli -v -w -mmcu=atmega32u4  ergodox_ez_jpike.hex
